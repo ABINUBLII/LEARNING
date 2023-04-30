7d76c2460b95e139a5fc3f3bc0f2b3b2a9cd7388
boris monteiro <borismonteiro@MacBook-Pro-boris.local>
2023-04-30 19:58:20 +0700

    array

--- ARRAY/array.js
+++ ARRAY/array.js
@@ -4,3 +4,10 @@ const friend3 = `hilmi`;
 
 const friends = [`abi`, `rahman`, `hilmi`];
 console.log(friends);
+console.log(friends[1]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+//ganti salah satu nama dari 3 orang tersebut
+friends[2] = `nugi`;
+console.log(friends);
