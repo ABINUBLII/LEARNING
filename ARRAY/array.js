d552f6a8226877d368a7db26577d7959bbc94e01
boris monteiro <borismonteiro@MacBook-Pro-boris.local>
2023-05-01 17:15:30 +0700

    exercize

--- ARRAY/array.js
+++ ARRAY/array.js
@@ -16,3 +16,19 @@ const firstName = `abi`;
 const abi = [firstName, `nubli`, 2023 - 1997, `employee`, friends];
 
 console.log(abi);
+
+//exercize
+
+const calcAge = function (birthYear) {
+  return 2050 - birthYear;
+};
+
+const years = [50, 100, 1000, 1500, 2000];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+
+console.log(ages);
