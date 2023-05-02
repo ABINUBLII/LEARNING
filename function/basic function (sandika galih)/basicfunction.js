3364913e276314824d59a26e686bfd92b01598d6
ABINUBLII <nubliabi97@gmail.com>
2023-05-02 14:44:54 +0700

    sandika galih, macam macam input data di basic FUNCTION

--- function/basic function (sandika galih)/basicfunction.js
+++ function/basic function (sandika galih)/basicfunction.js
@@ -1,18 +1,40 @@
 //hitung volume kubus dengan rumus a * a, b*b. c * c
 
-function hitungVolumeKubus(a, b, c) {
-  let volumeA;
-  let volumeB;
-  let volumeC;
-  let total;
+// function hitungVolumeKubus(a, b, c) {
+//   let volumeA;
+//   let volumeB;
+//   let volumeC;
+//   let total;
+//   volumeA = a * a;
+//   volumeB = b * b;
+//   volumeC = c * c;
 
-  volumeA = a * a;
-  volumeB = b * b;
-  volumeC = c * c;
+//   total = volumeA + volumeB + volumeC;
 
-  total = volumeA + volumeB + volumeC;
+//   return total;
+// }
 
-  return total;
+// const hasil = hitungVolumeKubus(1, 2, 5);
+// console.log(hasil);
+
+//>>>>>jamming session<<<<<
+// 1. bisa gini
+
+function tambah(a, b) {
+  return a + b;
 }
 
-console.log(hitungVolumeKubus(1, 2, 3));
+const hasil = tambah(10, 10);
+console.log(hasil);
+
+// 2 bisa juga gini
+
+function tambah1(d, e) {
+  return d + e;
+}
+const valueA = 5;
+const valueB = 5;
+
+const hasil1 = tambah1(valueA, valueB);
+
+console.log(hasil1);
