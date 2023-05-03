5542efb943c7a82cb9d20d3defd8397aa84b7889
ABINUBLII <nubliabi97@gmail.com>
2023-05-03 13:10:36 +0700

    basic array sandika galih

---
+++ function/array sandika galih/arrSandika.js
@@ -0,0 +1,27 @@
+// const angka = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// for( const i = 0; < angka.length; i++ ) {
+//     console.log (angka[i])
+// }
+
+// 4.splice
+// >>>splice(index awal(untuk yang mau ditambah), maudihapusberapa)<<<
+
+let nama0 = [`abi`, `rahman`, `hilmi`];
+nama0.splice(3, 0, `gomi`);
+console.log(nama0);
+
+let nama1 = [`abi`, `rahman`, `hilmi`];
+nama1.splice(1, 0, `nubli`);
+console.log(nama1);
+
+let nama2 = [`abi`, `rahman`, `hilmi`];
+nama2.splice(1, 2, `nubli`, `farandi`);
+console.log(nama2);
+
+//slice
+// menghasilkan array baru, jadi harus ditampung kedalam variable.
+// >>> slice (awal, akhir)
+let nama3 = [`abi`, `rahman`, `hilmi`, `farandi`, `gomi`, `nubli`]; //abi (0), rahman (1), hilmi (2), farandi (3), dst.
+const namaa3 = nama3.slice(0);
+console.log(namaa3);
